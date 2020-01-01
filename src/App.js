a919427299b8add7958928f74dbd657140d97631
Haris Dolovac <dharis90@gmail.com>
2020-01-01 03:15:01 +0100

    update menu-items and add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import HomePage from "./pages/homepage/Homepage.component";
+import { Route, Switch } from "react-router-dom";
+import Hats from "./pages/Hatspage.component";
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
+      </Switch>
     </div>
   );
 };
---
+++ src/pages/Hatspage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hats = () => {
+  return (
+    <div>
+      <p>Hats Page</p>
+    </div>
+  );
+};
+
+export default Hats;
